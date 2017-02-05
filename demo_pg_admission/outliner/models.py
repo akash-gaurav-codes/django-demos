c206d930f168f83f610111490ef88973892a4301
Akash Gaurav <akash.gaurav.coder@gmail.com>
2017-02-05 11:17:35 +0530

    Added outliner model fields and created final template.The django admin interface can be used to interact with Entry model

--- demo_pg_admission/outliner/models.py
+++ demo_pg_admission/outliner/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+	heading = models.CharField(max_length=140)
+	text    = models.TextField(blank=True)
+	parent  = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+	created = models.DateTimeField(auto_now_add = True)
+	updated = models.DateTimeField(blank=True)
+	deleted = models.BooleanField(default=False)
+
+	def __str__(self):
+		return (self.heading)
