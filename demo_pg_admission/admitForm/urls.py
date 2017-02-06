f201c1c25adb18dead30e639845f648af8e25a79
Akash Gaurav <akash.gaurav.coder@gmail.com>
2017-02-06 12:17:02 +0530

    Fully functional admission form

--- demo_pg_admission/admitForm/urls.py
+++ demo_pg_admission/admitForm/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-				url(r'', views.showForm, name='showForm')
+				url(r'^$', views.showForm, name='showForm'),
+				url(r'^save/$', views.save, name='save')
 				]
--- demo_pg_admission/admitForm/views.py
+++ demo_pg_admission/admitForm/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import User, ExtraCurricularActivity
+
 # Create your views here.
 def showForm(request):
-	return HttpResponse("Show the form here")
+	return render(request, "admitForm/admitForm.html")
+
+def save(request):
+	print("hi there")
+	user = User.objects.create(
+				name    = request.POST['name'],
+				gender  = request.POST['gender'], 
+				mobile  = request.POST['mobile'], 
+				age     = request.POST['age'], 
+				address = request.POST['address'], 
+				email   = request.POST['email'],
+				boardX = request.POST['XthBoard'],
+				streamX = request.POST['XthStream'],
+				marksX = request.POST['XthMarks'],
+				yearX = request.POST['XthYear'],
+				boardXII = request.POST['XIIthBoard'],
+				streamXII = request.POST['XIIthStream'],
+				marksXII = request.POST['XIIthMarks'],
+				yearXII = request.POST['XIIthYear'],
+				college = request.POST['College'],
+				course = request.POST['Course'],
+				cgpa = request.POST['CGPA'],
+				gradYear = request.POST['GradYear']
+	)
+					
+	i=1
+	while (request.POST.__contains__('activity'+str(i))):
+		eca = ExtraCurricularActivity.objects.create(
+			user = user,
+			activityName = request.POST['activity'+str(i)],
+			year = request.POST['yearActivity'+str(i)],
+			participationOrAward = request.POST['resultActivity'+str(i)]
+		)
+		print('eca created')
+		i = i+1
+
+ 		
+	return HttpResponse("Thank you. Your form has been submitted")	
